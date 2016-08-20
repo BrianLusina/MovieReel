3d8762289c7a35ac9532a6fa0c8de2dc29a63652
Brian Lusina <lusinabrian@gmail.com>
2016-08-20 09:17:47 +0300

    Add Main Activity and change theme color

---
+++ app/src/main/java/com/moviereel/moviereel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.moviereel.moviereel;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Project: Movie Reel
+ * Package: com.moviereel.moviereel
+ * Created by lusinabrian on 20/08/16 at 09:17
+ * <p/>
+ * Description:
+ */
+public class MainActivity extends AppCompatActivity{
+}
