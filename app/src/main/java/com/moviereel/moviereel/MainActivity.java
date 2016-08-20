0795557336f1411abaee36a70c7bf1e14cb0ebff
Brian Lusina <lusinabrian@gmail.com>
2016-08-20 09:20:14 +0300

    Add Main Activity layout

--- app/src/main/java/com/moviereel/moviereel/MainActivity.java
+++ app/src/main/java/com/moviereel/moviereel/MainActivity.java
@@ -1,5 +1,7 @@
 package com.moviereel.moviereel;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,4 +12,12 @@ import android.support.v7.app.AppCompatActivity;
  * Description:
  */
 public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity_layout);
+    }
+
+/*END*/
 }
