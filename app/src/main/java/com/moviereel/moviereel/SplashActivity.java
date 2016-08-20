132451531366e84d1ed7785b290a391171d1c09d
Brian Lusina <lusinabrian@gmail.com>
2016-08-20 08:57:56 +0300

    Add Splash screen.
    Add splash screen avtivity, The activity will sleep after 3 seconds and then display the Main activity which will contain the List for the movies

--- app/src/main/java/com/moviereel/moviereel/SplashActivity.java
+++ app/src/main/java/com/moviereel/moviereel/SplashActivity.java
@@ -1,13 +1,65 @@
 package com.moviereel.moviereel;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    /*FIELDS*/
+    private TextView appName, appTag;
+    private ImageView appIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.splash_activity_layout);
+
+        initializeUICtrls();
+
+        //set the timer
+        Thread timer = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException ie){
+                    ie.printStackTrace();
+                }
+                Intent openMain = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(openMain);
+            }
+        };
+        //start timer
+        timer.start();
+
     }
-}
+
+    /**Initializes the User Interface controls
+     * obtains the appName, appTag and appIcon variables and finds their ids in the layout*/
+    private void initializeUICtrls() {
+        appIcon = (ImageView)findViewById(R.id.appicon_splash);
+        appName = (TextView)findViewById(R.id.appname_splash);
+        appTag = (TextView)findViewById(R.id.apptag_splash);
+
+        //set the fonts
+        String fontPath = "fonts/Roboto-Black.ttf";
+        Typeface typeface = Typeface.createFromAsset(getAssets(), fontPath);
+        appName.setTypeface(typeface);
+    }
+
+    /*kill this splash screen to save memory*/
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+}/*END*/
