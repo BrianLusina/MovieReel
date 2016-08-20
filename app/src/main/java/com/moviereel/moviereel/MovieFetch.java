17a46df2987e3f76dc18342a488da2d013b1e3e1
Brian Lusina <lusinabrian@gmail.com>
2016-08-20 10:42:21 +0300

    Add Movie fetch to fetch from API

---
+++ app/src/main/java/com/moviereel/moviereel/MovieFetch.java
@@ -0,0 +1,11 @@
+package com.moviereel.moviereel;
+
+/**
+ * Project: Movie Reel
+ * Package: com.moviereel.moviereel
+ * Created by lusinabrian on 20/08/16 at 10:41
+ * <p/>
+ * Description: Fetch movies from Apiary API
+ */
+public class MovieFetch {
+}
