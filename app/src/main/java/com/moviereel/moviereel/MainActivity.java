fc2f39c61e3226765249b12873712b4cd9de1ba9
Brian Lusina <lusinabrian@gmail.com>
2016-08-20 10:06:59 +0300

    Add isNetworkAvailable to check for internet connection

--- app/src/main/java/com/moviereel/moviereel/MainActivity.java
+++ app/src/main/java/com/moviereel/moviereel/MainActivity.java
@@ -1,19 +1,63 @@
 package com.moviereel.moviereel;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.List;
 
 /**
  * Project: Movie Reel
  * Package: com.moviereel.moviereel
  * Created by lusinabrian on 20/08/16 at 09:17
+ * Description: Contains the RecyclerView for the movie items
+ * Allows the user to refresh using a swipe refresh
  */
 public class MainActivity extends AppCompatActivity{
+    private static final  String FOODFRAG_TAG = MainActivity.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private MovieAdapter foodAdapter;
+    private List<MovieModel> foodModelList;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity_layout);
+
+        if(isNetworkAvailable()) {
+            loadNews.execute();
+        }else{
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, getString(R.string.snackbar_warning_no_internet_conn), Snackbar.LENGTH_SHORT)
+                    .setAction(getString(R.string.snackbar_no_internet_conn_retry), new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(coordinatorLayout, getString(R.string.snackbar_no_internet_conn_retry), Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+            snackbar.show();
+        }
+    }
+
+
+
+
+    /**
+     * Method to check network availability
+     Using ConnectivityManager to check for Network Connection
+     * */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null;
     }
 
 /*END*/
