eecafd880720d8d995e9c363e87376b991fb0925
Brian Lusina <lusinabrian@gmail.com>
2016-08-20 22:10:01 +0300

    Fetch JSON data

--- app/src/main/java/com/moviereel/moviereel/MovieFetch.java
+++ app/src/main/java/com/moviereel/moviereel/MovieFetch.java
@@ -1,4 +1,9 @@
 package com.moviereel.moviereel;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.List;
 import java.io.IOException;
@@ -18,23 +23,19 @@ import okhttp3.OkHttpClient;
  * params – Any parameters you want to submit to that url. This is optional.
  */
 public class MovieFetch{
-    protected final String KEY = "2f30bdb7e9742c26d4ea364f62f38163";
-    protected final String BASEURL="https://api.themoviedb.org/3/";
+    protected final static String KEY = "?api_key=2f30bdb7e9742c26d4ea364f62f38163";
+    protected final static String BASEURL="https://api.themoviedb.org/3/";
 
     /*url to fetch GENRES*/
-    protected final String GENRES = BASEURL + "/genre/movie/list?" + KEY;
+    protected final static String GENRES = BASEURL + "genre/movie/list" + KEY;
 
     /*get movies per genre add id to this link to get movies per genre*/
-    protected final String MOVIES_PER_GENRE = BASEURL + "genre/";
+    protected final static String MOVIES_PER_GENRE = BASEURL + "genre/";
     /*append this to above string to get list of movies*/
-    protected final String MOVIES_PER_GENRE_endpt = "movies?"+KEY;
+    protected final static String MOVIES_PER_GENRE_endpt = "movies"+KEY;
+    OkHttpClient client = new OkHttpClient();
 
-    static String response = null;
-    public final static int GET = 1;
-    public final static int POST = 2;
-    static OkHttpClient client = new OkHttpClient();
-
-
+    /*constructor*/
     public MovieFetch() {}
 
     public String run(String url) throws IOException {
@@ -49,6 +50,7 @@ public class MovieFetch{
     public String fetchAllGenres() throws IOException {
        return run(GENRES);
     }
+
 
 /*END*/
 }
