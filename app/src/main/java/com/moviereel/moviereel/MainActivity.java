0a91d01f759f88f8f50ca24b5854357453afd8f8
Brian Lusina <lusinabrian@gmail.com>
2016-08-21 22:01:37 +0300

    add layout for latest movies

--- app/src/main/java/com/moviereel/moviereel/MainActivity.java
+++ app/src/main/java/com/moviereel/moviereel/MainActivity.java
@@ -3,30 +3,12 @@ package com.moviereel.moviereel;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.OvershootInterpolator;
-
+import android.support.v7.widget.Toolbar;
 import com.mikepenz.materialdrawer.Drawer;
-import com.moviereel.moviereel.movies.MovieAdapter;
-import com.moviereel.moviereel.movies.MovieFetch;
-import com.moviereel.moviereel.movies.MovieModel;
-
-import java.io.IOException;
-import java.util.List;
-
-import jp.wasabeef.recyclerview.animators.LandingAnimator;
-import jp.wasabeef.recyclerview.animators.adapters.AlphaInAnimationAdapter;
-import jp.wasabeef.recyclerview.animators.adapters.ScaleInAnimationAdapter;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 
 /**
  * Project: Movie Reel
@@ -38,80 +20,32 @@ import jp.wasabeef.recyclerview.animators.adapters.ScaleInAnimationAdapter;
 public class MainActivity extends AppCompatActivity{
     private static final  String MAINACTIVITY_TAG = MainActivity.class.getSimpleName();
     private Drawer drawer;
-    private RecyclerView recyclerView;
-    private MovieAdapter movieAdapter;
-    private List<MovieModel> foodModelList;
-    private CoordinatorLayout coordinatorLayout;
-    private LoadMoviesTask loadMovies;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainactivity_layout);
         initUiCtrls();
-        if(isNetworkAvailable()) {
-            loadMovies.execute();
-        }else{
-            Snackbar snackbar = Snackbar
-                    .make(coordinatorLayout, getString(R.string.snackbar_warning_no_internet_conn), Snackbar.LENGTH_SHORT)
-                    .setAction(getString(R.string.snackbar_no_internet_conn_retry), new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Snackbar snackbar1 = Snackbar.make(coordinatorLayout, getString(R.string.snackbar_no_internet_conn_retry), Snackbar.LENGTH_SHORT);
-                            snackbar1.show();
-                        }
-                    });
-            snackbar.show();
-        }
+
+        //this layout have to contain child layouts
+        drawer = new DrawerBuilder(this)
+                .withToolbar(mToolbar)
+                .withDisplayBelowStatusBar(false)
+                .withRootView(R.id.drawer_container)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.)
+                )
+                .withSavedInstance(savedInstanceState)
+                .build();
     }
 
     private void initUiCtrls() {
-        recyclerView = (RecyclerView) findViewById(R.id.main_recyclerview_id);
-        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.movie_coordinator_layout);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_id);
+        setSupportActionBar(mToolbar);
 
-        SwipeRefreshLayout mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.main_swipe_refresh_layout_id);
-        mSwipeRefreshLayout.setColorSchemeResources(
-                R.color.dark_slate_blue,
-                R.color.dark_slate_gray,
-                R.color.dark_cyan,
-                R.color.dark_yellow,
-                R.color.dark_turquoise,
-                R.color.dark_sea_green);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(movieAdapter);
-        AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(movieAdapter);
-        alphaAdapter.setInterpolator(new OvershootInterpolator());
-        ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(alphaAdapter);
-        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(mLinearLayoutManager);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setItemAnimator(new LandingAnimator());
-        recyclerView.setAdapter(scaleAdapter);
     }
 
-    /***
-     * Method to load Bonds from NSE and post them to the UI
-     */
-    private class LoadMoviesTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            MovieFetch movieFetch = new MovieFetch();
-            try {
-                Log.d(MAINACTIVITY_TAG, movieFetch.fetchAllGenres());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-        }
-    }
 
     /**
      * Method to check network availability
@@ -123,5 +57,22 @@ public class MainActivity extends AppCompatActivity{
         return activeNetworkInfo != null;
     }
 
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = drawer.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (drawer != null && drawer.isDrawerOpen()) {
+            drawer.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
 /*END*/
 }
