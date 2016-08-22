3a501d65738421112b43ee52b848595d684b0b7f
Brian Lusina <lusinabrian@gmail.com>
2016-08-22 20:51:51 +0300

    Update Movie Latest

--- app/src/main/java/com/moviereel/moviereel/movies/MovieLatest.java
+++ app/src/main/java/com/moviereel/moviereel/movies/MovieLatest.java
@@ -1,6 +1,32 @@
 package com.moviereel.moviereel.movies;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
+
+import com.moviereel.moviereel.R;
+
+import java.io.IOException;
+import java.util.List;
+
+import jp.wasabeef.recyclerview.animators.LandingAnimator;
+import jp.wasabeef.recyclerview.animators.adapters.AlphaInAnimationAdapter;
+import jp.wasabeef.recyclerview.animators.adapters.ScaleInAnimationAdapter;
 
 /**
  * Project: Movie Reel
@@ -10,12 +36,101 @@ import android.support.v4.app.Fragment;
  * Description: displays the latest movies
  */
 public class MovieLatest extends Fragment{
+    private static final String MOVIELATEST_TAG = MovieLatest.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private List<MovieModel> foodModelList;
+    private CoordinatorLayout coordinatorLayout;
+    private LoadMoviesTask loadMovies;
+
     public MovieLatest(){}
 
     public static Fragment newInstance(){
         MovieLatest movieLatest = new MovieLatest();
         movieLatest.setRetainInstance(true);
         return movieLatest;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(isNetworkAvailable()) {
+            loadMovies.execute();
+        }else{
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, getString(R.string.snackbar_warning_no_internet_conn), Snackbar.LENGTH_SHORT)
+                    .setAction(getString(R.string.snackbar_no_internet_conn_retry), new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(coordinatorLayout, getString(R.string.snackbar_no_internet_conn_retry), Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+            snackbar.show();
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.movielatest_layout, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.main_recyclerview_id);
+        coordinatorLayout = (CoordinatorLayout) rootView.findViewById(R.id.movie_coordinator_layout);
+
+        SwipeRefreshLayout mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.main_swipe_refresh_layout_id);
+
+        mSwipeRefreshLayout.setColorSchemeResources(
+                R.color.dark_slate_blue,
+                R.color.dark_slate_gray,
+                R.color.dark_cyan,
+                R.color.dark_yellow,
+                R.color.dark_turquoise,
+                R.color.dark_sea_green);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+        AlphaInAnimationAdapter alphaAdapter = new AlphaInAnimationAdapter(movieAdapter);
+        alphaAdapter.setInterpolator(new OvershootInterpolator());
+        ScaleInAnimationAdapter scaleAdapter = new ScaleInAnimationAdapter(alphaAdapter);
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLinearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new LandingAnimator());
+        recyclerView.setAdapter(scaleAdapter);
+        return rootView;
+    }
+
+    /**
+     * Method to check network availability
+     Using ConnectivityManager to check for Network Connection
+     * */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null;
+    }
+
+    /***
+     * Method to load Bonds from NSE and post them to the UI
+     */
+    private class LoadMoviesTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            MovieFetch movieFetch = new MovieFetch();
+            try {
+                Log.d(MOVIELATEST_TAG, movieFetch.fetchAllGenres());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 
 /*end*/
