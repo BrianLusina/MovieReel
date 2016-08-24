723972e13152d34ef8dd9e8409e09201da449621
Brian Lusina <lusinabrian@gmail.com>
2016-08-24 08:34:15 +0300

    Add sweet alert dialog dependency

--- app/src/main/java/com/moviereel/moviereel/movies/MovieNowPlaying.java
+++ app/src/main/java/com/moviereel/moviereel/movies/MovieNowPlaying.java
@@ -1,5 +1,6 @@
 package com.moviereel.moviereel.movies;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
