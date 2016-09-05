34957627072f1a30ddcb214c311e0bb762006e52
Brian Lusina <lusinabrian@gmail.com>
2016-09-05 14:55:02 +0300

    Add endpoints to gradle.properties

---
+++ app/src/main/java/com/moviereel/moviereel/models/TvModel.java
@@ -0,0 +1,11 @@
+package com.moviereel.moviereel.models;
+
+/**
+ * Project: Movie Reel
+ * Package: com.moviereel.moviereel.models
+ * Created by lusinabrian on 05/09/16 at 14:44
+ * <p/>
+ * Description:
+ */
+public class TvModel {
+}
