f6d96e8e991282449c30e31fe3fd7157821fd6d2
Brian Lusina <lusinabrian@gmail.com>
2016-09-05 15:43:30 +0300

    Add app introduction class

--- README.md
+++ README.md
@@ -12,7 +12,14 @@ ___
 
 ## Project Requirements
 
-+ Movie DB API
++ __Movie DB API__
+    
     The project requires on Movie DB API to fetch data about movies and tv shows. An API key is needed. This key is used to retrieve data from MovieDB API.
     
     It is recommended that the key is stored in your `gradle.properties` file (which should not be pushed to Github, or any other VCS) after which add them to your contracts class,or constants class before including them in your `build.gradle` file at the app level.
+    
++ __Paolo Ratolo's App Intro dependency__
+    
+    App Intro allows simple and short introduction into the application. They are only shown once on 1st ever start of the app. After which the splash screen will be shown for a brief 2 second.
+    
+    
--- app/src/main/java/com/moviereel/moviereel/views/introduction/AppIntroduction.java
+++ app/src/main/java/com/moviereel/moviereel/views/introduction/AppIntroduction.java
@@ -1,11 +1,61 @@
 package com.moviereel.moviereel.views.introduction;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.moviereel.moviereel.R;
+
 /**
  * Project: Movie Reel
  * Package: com.moviereel.moviereel.views.introduction
  * Created by lusinabrian on 05/09/16 at 15:24
  * <p/>
- * Description:
+ * Description: Simple app introduction class
  */
-public class AppIntroduction {
+public class AppIntroduction extends AppIntro {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO: change app introduction
+        /*1st Fragment*/
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, getResources().getColor(R.color.cadet_blue)));
+
+        /*2nd Fragment*/
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, getResources().getColor(R.color.cadet_blue)));
+
+        /*3rd Fragment*/
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, getResources().getColor(R.color.cadet_blue)));
+
+        /*4th Fragment*/
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, getResources().getColor(R.color.cadet_blue)));
+
+        setBarColor(ContextCompat.getColor(this,R.color.light_purple));
+        setSeparatorColor(ContextCompat.getColor(this,R.color.rebecca_purple));
+
+        showSkipButton(true);
+        setProgressButtonEnabled(true);
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+    }
+
 }
