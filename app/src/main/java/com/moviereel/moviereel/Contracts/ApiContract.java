5497d43621b5a3dc286fcbeb95dbcb144b63d63e
Brian Lusina <lusinabrian@gmail.com>
2016-09-05 13:01:09 +0300

    Add contracts

---
+++ app/src/main/java/com/moviereel/moviereel/Contracts/ApiContract.java
@@ -0,0 +1,12 @@
+package com.moviereel.moviereel.Contracts;
+
+/**
+ * Project: Movie Reel
+ * Package: com.moviereel.moviereel.Contracts
+ * Created by lusinabrian on 05/09/16 at 12:45
+ * <p/>
+ * Description: contains constants for the MovieDB api
+ */
+public class ApiContract {
+
+}
---
+++ app/src/main/java/com/moviereel/moviereel/Contracts/TvContract.java
@@ -0,0 +1,11 @@
+package com.moviereel.moviereel.Contracts;
+
+/**
+ * Project: Movie Reel
+ * Package: com.moviereel.moviereel.Contracts
+ * Created by lusinabrian on 05/09/16 at 13:00
+ * <p/>
+ * Description: Defines db/ shared prefs for TV shows
+ */
+public class TvContract {
+}
