1dbccfe2a57cddcad9c05f9f6bc1d8de6e300cc3
Brian Lusina <lusinabrian@gmail.com>
2016-09-05 15:06:46 +0300

    Add movie endpoints

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ ___
 + Each movie item is displayed using an ImageView and text about the movie.
 + Uses Coordinator Layout to enable refreshing of data
 + Source of movies is from [Apiary API](https://www.themoviedb.org)
+
+## Project Requirements
+
++ Movie DB API
+    The project requires on Movie DB API to fetch data about movies and tv shows. An API key is needed. This key is used to retrieve data from MovieDB API.
+    
--- app/src/main/java/com/moviereel/moviereel/Contracts/ApiContract.java
+++ app/src/main/java/com/moviereel/moviereel/Contracts/ApiContract.java
@@ -1,5 +1,7 @@
 package com.moviereel.moviereel.Contracts;
 
+import com.moviereel.moviereel.BuildConfig;
+
 /**
  * Project: Movie Reel
  * Package: com.moviereel.moviereel.Contracts
@@ -8,5 +10,12 @@ package com.moviereel.moviereel.Contracts;
  * Description: contains constants for the MovieDB api
  */
 public class ApiContract {
-
+    /*Movie enpoints*/
+    public static final String MOVIE_DB_KEY = BuildConfig.MOVIE_DB_KEY;
+    public static final String BASE_URL = BuildConfig.BASE_URL;
+    public static final String NOW_PLAYING = BASE_URL + BuildConfig.NOW_PLAYING + MOVIE_DB_KEY;
+    public static final String POPULAR_MOVIES = BASE_URL + BuildConfig.POPULAR_MOVIES + MOVIE_DB_KEY;
+    public static final String TOP_RATED = BASE_URL + BuildConfig.TOP_RATED + MOVIE_DB_KEY;
+    public static final String UPCOMING = BASE_URL + BuildConfig.UPCOMING + MOVIE_DB_KEY;
+    public static final String LATEST_MOVIE = BASE_URL + BuildConfig.LATEST_MOVIE+ MOVIE_DB_KEY;
 }
