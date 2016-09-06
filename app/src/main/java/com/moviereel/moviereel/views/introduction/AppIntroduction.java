07bd3b0166e68b04744b0b3eef93ef601e0ce7bc
Brian Lusina <lusinabrian@gmail.com>
2016-09-06 09:19:07 +0300

    Edit Introduction class
    Change pane background colors
    Start Main activity when eitheer Done or Skipped is pressed

--- app/src/main/java/com/moviereel/moviereel/views/introduction/AppIntroduction.java
+++ app/src/main/java/com/moviereel/moviereel/views/introduction/AppIntroduction.java
@@ -1,5 +1,6 @@
 package com.moviereel.moviereel.views.introduction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.support.v4.content.ContextCompat;
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.moviereel.moviereel.R;
+import com.moviereel.moviereel.views.MainActivity;
 
 /**
  * Project: Movie Reel
@@ -23,16 +25,16 @@ public class AppIntroduction extends AppIntro2 {
 
         //TODO: change app introduction
         /*1st Fragment*/
-        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "Reeling in the shows", R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.cadet_blue)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "Reeling in the shows", R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.dark_slate_blue)));
 
         /*2nd Fragment*/
-        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.cadet_blue)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.indigo_600)));
 
         /*3rd Fragment*/
-        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.cadet_blue)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "desc", R.mipmap.ic_launcher, ContextCompat.getColor(this,R.color.indigo_500)));
 
         /*4th Fragment*/
-        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "GET STARTED", R.mipmap.ic_launcher,ContextCompat.getColor(this,R.color.cadet_blue)));
+        addSlide(AppIntroFragment.newInstance(getString(R.string.app_name), "GET STARTED", R.mipmap.ic_launcher,ContextCompat.getColor(this,R.color.md_indigo_400)));
         setProgressButtonEnabled(true);
 
         /*animation*/
@@ -42,12 +44,14 @@ public class AppIntroduction extends AppIntro2 {
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
+        openMain();
         finish();
     }
 
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
+        openMain();
         finish();
     }
 
@@ -56,4 +60,9 @@ public class AppIntroduction extends AppIntro2 {
         super.onSlideChanged(oldFragment, newFragment);
     }
 
+    /**open main activity*/
+    public void openMain(){
+        Intent openMain = new Intent(AppIntroduction.this, MainActivity.class);
+        startActivity(openMain);
+    }
 }
