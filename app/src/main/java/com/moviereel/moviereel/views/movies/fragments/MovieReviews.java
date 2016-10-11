e14dd1c1ac1d54bc3c43f2382f4486aac8f46626
BrianLusina <lusinabrian@gmail.com>
2016-10-11 12:39:56 +0300

    Create MovieReview Fragment lifecycle

--- app/src/main/java/com/moviereel/moviereel/views/movies/fragments/MovieReviews.java
+++ app/src/main/java/com/moviereel/moviereel/views/movies/fragments/MovieReviews.java
@@ -3,11 +3,18 @@ package com.moviereel.moviereel.views.movies.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.moviereel.moviereel.R;
+import com.moviereel.moviereel.models.MovieModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.moviereel.moviereel.models.Contract.MOVIE_PARCEL_KEY;
 
 /**
  * MovieReel
@@ -17,6 +24,7 @@ import com.moviereel.moviereel.R;
  */
 public class MovieReviews extends Fragment{
     public static final String MOVIEREVIEWS_TAG = MovieReviews.class.getSimpleName();
+    @BindView(R.id.moviereview_recyclerView) RecyclerView reviewRecycler;
 
     public MovieReviews(){}
 
@@ -26,11 +34,46 @@ public class MovieReviews extends Fragment{
         return movieReviews;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //retrieve the arguments and set them to the movie model
+        Bundle bundle = getArguments();
+        MovieModel movieModel = bundle.getParcelable(MOVIE_PARCEL_KEY);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.moviereviews_layout, container, false);
-
+        ButterKnife.bind(this, rootView);
         return rootView;
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
 }
