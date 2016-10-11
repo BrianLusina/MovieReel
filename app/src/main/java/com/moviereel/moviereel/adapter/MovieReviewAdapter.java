b03562d04d624e8bd8798340c2e3283e6f2be85a
BrianLusina <lusinabrian@gmail.com>
2016-10-11 12:25:30 +0300

    Add ReviewsModel adapter

--- app/src/main/java/com/moviereel/moviereel/adapter/MovieReviewAdapter.java
+++ app/src/main/java/com/moviereel/moviereel/adapter/MovieReviewAdapter.java
@@ -1,13 +1,13 @@
 package com.moviereel.moviereel.adapter;
 
 import android.content.Context;
-import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
@@ -20,20 +20,20 @@ import java.util.List;
 /**
  * MovieReel
  * com.moviereel.moviereel.adapter
- * Created by lusinabrian on 10/10/16.
- * Description: RecyclerAdapter for the Images of the movie or TVShow
+ * Created by lusinabrian on 11/10/16.
+ * Description: Adapter for the review fragment
  */
 
-public class MovieImageAdapter extends RecyclerView.Adapter<MovieImageAdapter.ViewHolder>{
+public class MovieReviewAdapter extends RecyclerView.Adapter<MovieReviewAdapter.ViewHolder>{
     private Context mContext;
-    private List<ImagesModel> imagesModelsList;
+    private List<ReviewsModel> reviewsModelList;
     public int itemLayout;
 
     // constructor
-    public MovieImageAdapter(Context mContext, List<ImagesModel> imagesModelList, int itemLayout){
+    public MovieReviewAdapter(Context mContext, List<ReviewsModel> reviewsModelList, int itemLayout){
         this.mContext = mContext;
         this.itemLayout = itemLayout;
-        this.imagesModelsList = imagesModelList;
+        this.reviewsModelList = reviewsModelList;
         this.notifyDataSetChanged();
     }
 
@@ -45,12 +45,12 @@ public class MovieImageAdapter extends RecyclerView.Adapter<MovieImageAdapter.Vi
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        ImagesModel imagesModel = imagesModelsList.get(position);
-        holder.itemView.setTag(imagesModel);
-        holder.bind(imagesModel);
+        ReviewsModel reviewsModel = reviewsModelList.get(position);
+        holder.itemView.setTag(reviewsModel);
+        holder.bind(reviewsModel);
         //load images using Glider library
         Glide.with(mContext)
-                .load(imagesModel.getFilePath())
+                .load(reviewsModel.getFilePath())
                 .listener(new RequestListener<String, GlideDrawable>() {
                     @Override
                     public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
@@ -70,7 +70,7 @@ public class MovieImageAdapter extends RecyclerView.Adapter<MovieImageAdapter.Vi
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
-        public ImageView  moviePoster;
+        public ImageView moviePoster;
         public ProgressBar progressBar;
 
         public ViewHolder(View itemView) {
@@ -87,16 +87,15 @@ public class MovieImageAdapter extends RecyclerView.Adapter<MovieImageAdapter.Vi
         }
     }
 
-    public void add(ImagesModel itemModel, int position){
-        imagesModelsList.add(position,itemModel);
+    public void add(ReviewsModel itemModel, int position){
+        reviewsModelList.add(position,itemModel);
         notifyDataSetChanged();
     }
 
 
     @Override
     public int getItemCount() {
-        return imagesModelsList.size();
+        return reviewsModelList.size();
     }
 
 }
-
