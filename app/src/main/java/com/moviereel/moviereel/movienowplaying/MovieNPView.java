79bc42150ee604ca1198392bd8cec0e48a424c22
BrianLusina <lusinabrian@gmail.com>
2016-10-26 15:51:07 +0300

    Repackaged files

--- app/src/main/java/com/moviereel/moviereel/movienowplaying/MovieNPView.java
+++ app/src/main/java/com/moviereel/moviereel/movienowplaying/MovieNPView.java
@@ -1,5 +1,9 @@
 package com.moviereel.moviereel.movienowplaying;
 
+import com.moviereel.moviereel.models.MovieModel;
+
+import java.util.List;
+
 /**
  * MovieReel
  * com.moviereel.moviereel.movienowplaying
@@ -9,4 +13,18 @@ package com.moviereel.moviereel.movienowplaying;
 
 interface MovieNPView {
 
+    /**handles displaying the progress dialog when fetching Now Playing items*/
+    void showProgress();
+
+    /**Dismisses the progress dialog when done fetching Now Playing items*/
+    void dismissProgress();
+
+    /**In case of any error, display a message to the user
+     * @param message The message to display to the user
+     * @param messageType The message type to display, whether an error or informative*/
+    void displayToast(String message, int messageType);
+
+    /**responsible for setting the items to the RecyclerView
+     * @param movieModelList The List of Movie Objects fetched from Model layer*/
+    void setItems(List<MovieModel> movieModelList);
 }
