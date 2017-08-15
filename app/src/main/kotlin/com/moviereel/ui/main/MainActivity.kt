e7e6c7c8ad6f8dd2ae1fd27980d900fcce54082f
Brian Lusina <lusinabrian@gmail.com>
2017-08-15 20:46:50 +0300

    layout restructuring
    Update layout structure to features
    Allows for easier scaling of a particular feature set and updating it without affecting other components of app
    delete unused resources that were duplicates

--- app/src/main/kotlin/com/moviereel/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/moviereel/ui/main/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : BaseActivity(), MainView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_layout)
+        setContentView(R.layout.activity_main)
 
         activityComponent?.inject(this)
 
