ef0a49cb90c693c5a84fa3b5fcaa7ea123b1929f
Brian Lusina <lusinabrian@gmail.com>
2017-09-13 10:04:44 +0300

    Create base page adapter

--- app/src/main/kotlin/com/moviereel/ui/entertain/movie/nowplaying/MovieNPAdapter.kt
+++ app/src/main/kotlin/com/moviereel/ui/entertain/movie/nowplaying/MovieNPAdapter.kt
@@ -6,8 +6,8 @@ import android.view.ViewGroup
 import com.moviereel.BuildConfig
 import com.moviereel.R
 import com.moviereel.data.db.entities.movie.MovieNPEntity
-import com.moviereel.ui.base.BaseRecyclerAdapter
 import com.moviereel.ui.base.BaseViewHolder
+import com.moviereel.ui.entertain.base.EntertainPageBaseAdapter
 import com.moviereel.utils.loadImageFromUrl
 import kotlinx.android.synthetic.main.item_entertainment_layout.view.*
 import org.jetbrains.anko.toast
@@ -20,49 +20,29 @@ import javax.inject.Inject
 
 class MovieNPAdapter
 @Inject
-constructor(val movieNPEntityList: ArrayList<MovieNPEntity>) : BaseRecyclerAdapter<MovieNPEntity>(movieNPEntityList) {
+constructor(val movieNPEntityList: ArrayList<MovieNPEntity>) : EntertainPageBaseAdapter<MovieNPEntity>(movieNPEntityList) {
 
     private val VIEW_TYPE_LOADING = 0
     private val VIEW_TYPE_NORMAL = 1
 
-    lateinit var mCallback: Callback
-
-    fun setCallback(callback: Callback) {
-        mCallback = callback
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<MovieNPEntity> {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseViewHolder<MovieNPEntity> {
         return when (viewType) {
             VIEW_TYPE_NORMAL -> {
-                val v = LayoutInflater.from(parent.context).inflate(R.layout.item_entertainment_layout, parent, false)
+                val v = LayoutInflater.from(parent?.context).inflate(R.layout.item_entertainment_layout, parent, false)
                 ViewHolder(v)
             }
             VIEW_TYPE_LOADING -> {
-                val v = LayoutInflater.from(parent.context).inflate(R.layout.progress_dialog, parent, false)
+                val v = LayoutInflater.from(parent?.context).inflate(R.layout.progress_dialog, parent, false)
                 LoadingViewHolder(v)
             }
             else -> {
-                val v = LayoutInflater.from(parent.context).inflate(R.layout.item_empty_view, parent, false)
+                val v = LayoutInflater.from(parent?.context).inflate(R.layout.item_empty_view, parent, false)
                 EmptyViewHolder(v)
             }
         }
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return if (movieNPEntityList.size > 0) {
-            VIEW_TYPE_NORMAL
-        } else {
-            VIEW_TYPE_LOADING
-        }
-
-    }
-
-    override fun getItemCount(): Int {
-        return movieNPEntityList.size
-    }
-
     inner class ViewHolder(itemView: View) : BaseViewHolder<MovieNPEntity>(itemView) {
-
         override fun onBind(position: Int) {
             val movieEntity = movieNPEntityList[position]
 
@@ -89,22 +69,7 @@ constructor(val movieNPEntityList: ArrayList<MovieNPEntity>) : BaseRecyclerAdapt
 //                    )
                 }
             }
-
         }
-    }
-
-    inner class EmptyViewHolder(itemView: View) : BaseViewHolder<MovieNPEntity>(itemView) {
-
-        fun onRetryClick() {
-            // btn_retry
-            mCallback.onViewEmptyViewRetryClick()
-        }
-    }
-
-    inner class LoadingViewHolder(itemView: View) : BaseViewHolder<MovieNPEntity>(itemView)
-
-    interface Callback {
-        fun onViewEmptyViewRetryClick()
     }
 
 }
