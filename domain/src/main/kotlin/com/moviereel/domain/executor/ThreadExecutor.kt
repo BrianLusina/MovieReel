bc08cf9fbf2ae9e10ebae7b3bd140b3f2a02ce6b
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-04-24 08:11:12 +0300

    UseCase implementation

--- domain/src/main/kotlin/com/moviereel/domain/executor/ThreadExecutor.kt
+++ domain/src/main/kotlin/com/moviereel/domain/executor/ThreadExecutor.kt
@@ -1,7 +1,10 @@
 package com.moviereel.domain.executor
 
+import java.util.concurrent.Executor
+
 /**
  * @author lusinabrian on 18/04/18.
- * @Notes
+ * Executor implementation can be based on different frameworks or techniques of asynchronous
+ * execution, but every implementation will execute the [UseCase]
  */
-class ThreadExecutor
+interface ThreadExecutor : Executor
--- domain/src/main/kotlin/com/moviereel/domain/interactors/UseCase.kt
+++ domain/src/main/kotlin/com/moviereel/domain/interactors/UseCase.kt
@@ -2,7 +2,11 @@ package com.moviereel.domain.interactors
 
 import com.moviereel.domain.executor.PostExecutionThread
 import com.moviereel.domain.executor.ThreadExecutor
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 /**
  * @author lusinabrian on 18/04/18.
@@ -18,4 +22,42 @@ abstract class UseCase<T, in Params> constructor(private val threadExecutor: Thr
 
     private val disposables = CompositeDisposable()
 
+    /**
+     * Builds a use case observable used when the current UseCase is being executed.
+     * @param params
+     * @return [Single]
+     */
+    abstract fun buildUseCaseObservable(params: Params? = null) : Single<T>
+
+    /**
+     * Executes the current use case
+     * @param singleObserver a [DisposableSingleObserver] which will be listening to the [Single] build by
+     * [buildUseCaseObservable] method
+     * @param params (optional) used to build and execute this use case
+     */
+    open fun execute(singleObserver: DisposableSingleObserver<T>, params: Params? =null){
+        checkNotNull(singleObserver)
+        val single = buildUseCaseObservable(params)
+                .subscribeOn(Schedulers.from(threadExecutor))
+                .observeOn(postExecutionThread.scheduler) as Single<T>
+        addDisposable(single.subscribeWith(singleObserver))
+    }
+
+    /**
+     * Dispose from current [CompositeDisposable]
+     */
+    fun dispose(){
+        if(!disposables.isDisposed)
+            disposables.dispose()
+    }
+
+    /**
+     * add disposable to current [CompositeDisposable]
+     * @param disposable
+     */
+    private fun addDisposable(disposable : Disposable){
+        checkNotNull(disposable)
+        checkNotNull(disposables)
+        disposables.add(disposable)
+    }
 }
